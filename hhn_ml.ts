27b0f40aa8da6f6f3cea04272bf184b5ae85b559
simonschweizer <5512013+simonschweizer@users.noreply.github.com>
2019-03-18 16:58:59 +0100

    make it work on calliope platform

--- hhn_ml.ts
+++ hhn_ml.ts
@@ -19,13 +19,13 @@ namespace ml_knn {
         return instance.predict(temperature, 1);
     }
 }
-    
+
 
 
 //% color=190 weight=100 icon="\uf1ec" block="ml_dt"
 namespace ml_dt {
     let instance: SG = new SG();
-    let changed : boolean = true;
+    let changed: boolean = true;
 
     //% block
     export function train(temperature: number, location: Location): void {
@@ -103,103 +103,105 @@ class KNN {
         return best_class;
     }
 }
-    
+
 //% fixedInstances
 //% blockNamespace=ml_dt
 class SG {
-  list: number[][];
-  number_of_classes: number;
-  border : number = 0;
-  class_0_is_lower_then_border : boolean = true;
-  constructor() {
-    this.list = [];
-    this.number_of_classes = 2;
-  }
+    list: number[][];
+    number_of_classes: number;
+    border: number;
+    class_0_is_lower_then_border: boolean;
+    constructor() {
+        this.list = [];
+        this.number_of_classes = 2;
+        this.border = 0;
+        this.class_0_is_lower_then_border = true;
+    }
 
-  add(nbr: number, cls: number) {
-    this.list.push([nbr, cls]);
-  }
+    add(nbr: number, cls: number) {
+        this.list.push([nbr, cls]);
+    }
 
-  train() {
-    // todo
-    let calc_list: number[][] = [];
-    // remove nbr from each item
-    this.list.forEach(function (element) {
-        calc_list.push([element[0], element[1]]);
-    });
-    calc_list.sort(function (a, b) {
-        return a[0] - b[0];
-    });
-    console.log(calc_list);
-    let total_classification_error = calc_list.length;
-    let split_before : number = 0;
-    for (let i = 0; i <= calc_list.length; i++) {
-      // before
-      let count_0_before = 0;
-      let count_1_before = 0;
-      for (let j = 0; j < i; j++) {
-        if (calc_list[j][1]==0) {
-          count_0_before++;
+    train() {
+        // todo
+        let calc_list: number[][] = [];
+        // remove nbr from each item
+        this.list.forEach(function (element) {
+            calc_list.push([element[0], element[1]]);
+        });
+        calc_list.sort(function (a, b) {
+            return a[0] - b[0];
+        });
+        // console.log(calc_list);
+        let total_classification_error = calc_list.length;
+        let split_before: number = 0;
+        for (let i = 0; i <= calc_list.length; i++) {
+            // before
+            let count_0_before = 0;
+            let count_1_before = 0;
+            for (let j = 0; j < i; j++) {
+                if (calc_list[j][1] == 0) {
+                    count_0_before++;
+                } else {
+                    count_1_before++;
+                }
+            }
+            // after
+            let count_0_after = 0;
+            let count_1_after = 0;
+            for (let j = i; j < calc_list.length; j++) {
+                if (calc_list[j][1] == 0) {
+                    count_0_after++;
+                } else {
+                    count_1_after++;
+                }
+            }
+            // combined
+            let iteration_class_0_is_lower_then_border = true;
+            let iteration_classification_error = 0;
+            if ((count_0_after + count_1_after > count_0_before + count_1_before) || (count_0_before == count_1_before)) {
+                if (count_0_after > count_1_after) {
+                    iteration_class_0_is_lower_then_border = false;
+                }
+            } else {
+                if (count_0_before < count_1_before) {
+                    iteration_class_0_is_lower_then_border = false;
+                }
+            }
+
+            if (iteration_class_0_is_lower_then_border) {
+                iteration_classification_error = count_1_before + count_0_after;
+            } else {
+                iteration_classification_error = count_0_before + count_1_after;
+            }
+
+            if (iteration_classification_error < total_classification_error) {
+                total_classification_error = iteration_classification_error;
+                this.class_0_is_lower_then_border = iteration_class_0_is_lower_then_border;
+                split_before = i;
+                // console.log("Found better solution: split_before:" +split_before + ", <:" + this.class_0_is_lower_then_border + ", CE:" + total_classification_error);
+            } else {
+                // console.log("Found no better solution: split_before:" +i + ", <:" + iteration_class_0_is_lower_then_border + ", CE:" + iteration_classification_error);
+            }
+
+        }
+
+        if (split_before == 0 || split_before == calc_list.length) {
+            // console.log("no split in dataset...");
+            if (split_before == 0) {
+                this.border = -10000; // TODO replace with valid borders
+            } else {
+                this.border = 10000; // TODO replace with valid borders
+            }
         } else {
-          count_1_before++;
+            this.border = (calc_list[split_before - 1][0] + calc_list[split_before][0]) / 2;
+            // console.log("New border is " + this.border);
         }
-      }
-      // after
-      let count_0_after = 0;
-      let count_1_after = 0;
-      for (let j = i; j < calc_list.length; j++) {
-        if (calc_list[j][1]==0) {
-          count_0_after++;
-        } else {
-          count_1_after++;
-        }
-      }
-      // combined
-      let iteration_class_0_is_lower_then_border = true;
-      let iteration_classification_error = 0;
-      if ((count_0_after + count_1_after > count_0_before + count_1_before) || (count_0_before == count_1_before)) {
-        if (count_0_after > count_1_after){
-          iteration_class_0_is_lower_then_border = false;
-        }
-      } else {
-        if (count_0_before < count_1_before){
-          iteration_class_0_is_lower_then_border = false;
-        }
-      }
-
-      if (iteration_class_0_is_lower_then_border) {
-        iteration_classification_error = count_1_before + count_0_after;
-      } else {
-        iteration_classification_error = count_0_before + count_1_after;
-      }
-
-      if (iteration_classification_error < total_classification_error) {
-        total_classification_error = iteration_classification_error;
-        this.class_0_is_lower_then_border = iteration_class_0_is_lower_then_border;
-        split_before = i;
-        // console.log("Found better solution: split_before:" +split_before + ", <:" + this.class_0_is_lower_then_border + ", CE:" + total_classification_error);
-      } else {
-        // console.log("Found no better solution: split_before:" +i + ", <:" + iteration_class_0_is_lower_then_border + ", CE:" + iteration_classification_error);
-      }
 
     }
 
-    if (split_before == 0 || split_before == calc_list.length) {
-      // console.log("no split in dataset...");
-      if (split_before == 0) {
-        this.border = Number.NEGATIVE_INFINITY;
-      } else {
-        this.border = Number.MAX_SAFE_INTEGER;
-      }
-    } else {
-      this.border = (calc_list[split_before-1][0] + calc_list[split_before][0]) / 2;
-      // console.log("New border is " + this.border);
+    predict(nbr: number): number {
+        return ((this.class_0_is_lower_then_border && (nbr < this.border)) || (!this.class_0_is_lower_then_border && (nbr >= this.border))) ? 0 : 1;
     }
-    
-  }
-
-  predict(nbr: number): number {
-    return ((this.class_0_is_lower_then_border && (nbr < this.border)) || (!this.class_0_is_lower_then_border && (nbr >= this.border)))?0:1;
-  }
 
 }
